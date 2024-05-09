363356fa8496286a28f987bef6fea4cff591f64d
vitconlonton159 <147787911+vitconlonton159@users.noreply.github.com>
2024-05-09 10:38:23 +0700

    Update BstProductAdd.js

---
+++ src/components/BstProductAdd.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+class BstProductAdd extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            id:0,
+            title:'',
+            status:0
+        }
+    }
+
+    bstHandleChange = (event)=>{
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({
+            [name]:value
+        })
+    }
+    bstHandleSubmit = (ev)=>{
+        ev.preventDefault();
+
+        this.props.onSubmit(this.state);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2> them moi san pham</h2>
+                <form className='col-md-6'> 
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">
+                            ID
+                        </span>
+                        <input
+                        type="text"
+                        className="form-control"
+                        name='id'
+                        value={this.state.id}
+                        onChange={this.bstHandleChange}
+                        />
+                         
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon2">
+                            title
+                        </span>
+                        <input
+                        type="text"
+                        className="form-control"
+                        name='title'
+                        value={this.state.title}
+                        onChange={this.bstHandleChange}
+
+                        />
+                         
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon3">
+                            status
+                        </span>
+                        <input
+                        type="text"
+                        className="form-control"
+                        name='status'
+                        value={this.state.status}
+                        onChange={this.bstHandleChange}
+
+                        />
+                         
+                    </div>
+                    <button className='btn btn-success' onClick={this.bstHandleSubmit}>ghi lai</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default BstProductAdd;
